39b508434cd6e24db91b6f69ec0e20250b405d5b
Eric K <ericskao@gmail.com>
2023-03-02 01:46:46 -0800

    test

--- src/html.js
+++ src/html.js
@@ -7,10 +7,11 @@ export default function HTML(props) {
       <head>
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-        <meta
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+        {/* <meta
           name="viewport"
           content="width=device-width, initial-scale=1, minimum-scale=1"
-        />
+        /> */}
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
